94ecd75dd256179ecb9bfd8426e304e60f9bbae7
tt <516029781@qq.com>
2019-06-20 22:26:13 +0800

    ``

--- src/main/java/com/attend/dream/mapper/EmployeesMapper.java
+++ src/main/java/com/attend/dream/mapper/EmployeesMapper.java
@@ -7,24 +7,24 @@ import java.util.List;
 
 public interface EmployeesMapper {
     //查询所有的雇员
-    @Select("select empId,empCode,empName,empSex,empAge,empNation,empIdCard,empSalary,empTel,empContact,empCTel,empJobId,empNote\n" +
+    @Select("select empId,empCode,empName,empSex,empAge,empNation,empIdCard,empSalary,empTel,empContact,empCTel,empStaCode,empNote\n" +
             "from employees")
     List<Employee> getEmployees();
 
     //通过员工的名字查询
-    @Select("select empId,empCode,empName,empSex,empAge,empNation,empIdCard,empSalary,empTel,empContact,empCTel,empJobId,empNote\n" +
+    @Select("select empId,empCode,empName,empSex,empAge,empNation,empIdCard,empSalary,empTel,empContact,empCTel,empStaCode,empNote\n" +
             "from employees where empName = #{empName}")
     Employee getEmployeesByName(String empName);
 
     //添加员工
-    @Insert("insert into employees(empId,empCode,empName,empSex,empAge,empNation,empIdCard,empSalary,empTel,empContact,empCTel,empJobId,empNote)\n" +
-            "values(#{empId},#{empCode},#{empName},#{empSex},#{empAge},#{empNation},#{empIdCard},#{empSalary},#{empTel},#{empContact},#{empCTel},#{empJobId},#{empNote})")
+    @Insert("insert into employees(empId,empCode,empName,empSex,empAge,empNation,empIdCard,empSalary,empTel,empContact,empCTel,empStaCode,empNote)\n" +
+            "values(#{empId},#{empCode},#{empName},#{empSex},#{empAge},#{empNation},#{empIdCard},#{empSalary},#{empTel},#{empContact},#{empCTel},#{empStaCode},#{empNote})")
     @Options(useGeneratedKeys = true, keyProperty = "empId")
     Boolean insertEmployee(Employee e);
 
     //通过员工的id 修改员工的信息
     @Update("update employees set empId=#{empId},empCode=#{empCode},empName=#{empName},empSex=#{empSex},empAge=#{empAge},empNation=#{empNation},empIdCard=#{empIdCard},\n" +
-            "empSalary=#{empSalary},empTel=#{empTel},empContact=#{empContact},empCTel=#{empCTel},empJobId=#{empJobId},empNote=#{empNote}\n" +
+            "empSalary=#{empSalary},empTel=#{empTel},empContact=#{empContact},empCTel=#{empCTel},empStaCode=#{empStaCode},empNote=#{empNote}\n" +
             "where empId = #{empId}")
     Boolean updateEmployee(Employee e);
 
