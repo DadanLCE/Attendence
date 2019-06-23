0b8409f60401063b010546c7e7ee3599f554fc67
cc <516029781@qq.com>
2019-06-23 11:20:29 +0800

    department 查询

--- src/main/java/com/attend/dream/mapper/DepartmentMapper.java
+++ src/main/java/com/attend/dream/mapper/DepartmentMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
+
 public interface DepartmentMapper {
     //查询所有的部门
     @Select("select id,depCode,depJob,depName,depBossCode,depTopId\n" +
@@ -20,6 +21,25 @@ public interface DepartmentMapper {
     @Select("select id,depCode,depJob,depName,depBossCode,depTopId\n" +
             "from department where depCode LIKE CONCAT('%',#{depCode},'%') ")
     List<Department> getDepartmentsByDepCode(String depCode);
+
+
+
+
+    //模糊查询CodeMax
+    @Select("<script>"+
+            "select id,depCode,depJob,depName,depBossCode,depTopId from department" +
+            "<where>"+
+             "<if test='depCode != null'>"+
+             "and depCode LIKE '%${depCode}%'" +
+             "</if>"+
+            "</where>"+
+            "</script>")
+    List<Department> getDepartmentsByDepCodeMax(@Param("depCode") String depCode);
+//    @Select("select id,depCode,depJob,depName,depBossCode,depTopId\n" +
+//            "from department where depCode LIKE CONCAT('%',#{depCode},'%') ")
+
+    @Select("select id,depCode,depJob,depName,depBossCode,depTopId\n" +
+            "from department where depCode LIKE CONCAT('%',#{depCode},'%') ")
 
     //添加 部门
     @Insert("insert into department (depCode,depName,depBossCode,depJob,depTopId)\n" +
