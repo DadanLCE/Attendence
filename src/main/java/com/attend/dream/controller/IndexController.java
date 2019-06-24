30608816d6d3ce4240bf17f1a4d5aeab03392b9e
DandanLCE <www.chenjingq@qq.com>
2019-06-24 09:55:10 +0800

    主页嵌套employee

--- src/main/java/com/attend/dream/controller/IndexController.java
+++ src/main/java/com/attend/dream/controller/IndexController.java
@@ -2,12 +2,19 @@ package com.attend.dream.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-//    @GetMapping("/employee")
-//    public String gotoEmp() {
-//        return "employee_list";
-//    }
+
+    @RequestMapping(value = "/showEmp",method = RequestMethod.POST)
+    public String showEmp(){
+        return "employee_list";
+    }
+
+
+    @RequestMapping(value = "/showWel",method = RequestMethod.POST)
+    public String showWel(){ return "welcome";}
 }
