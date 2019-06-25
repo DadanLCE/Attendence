9cda90f0f128561b7e5c993c56b71110f2d85f59
fang <me@163.com>
2019-06-25 23:14:46 +0800

    6/25

--- src/main/java/com/attend/dream/mapper/CardEveMapper.java
+++ src/main/java/com/attend/dream/mapper/CardEveMapper.java
@@ -14,36 +14,21 @@ public interface CardEveMapper {
             "from cardEve where id = #{id}")
     CardEve getCardEveById(int id);
 
-//    //用于结算当天的出勤情况
-//    @Select("select id,cardCode,name,date,note" +
-//            "from card where cardCode = #{cardCode}")
-//    List<Card> getCardsToday(String cardCode);
-
-
 //    在打卡单  通过cardCode的 模糊查询
     @Select("select id, cardCode, name, date , note  " +
             "from cardEve where cardCode like concat('%',#{cardCode},'%') ")
     List<CardEve> getCardEvesByCode(String cardCode);
-
 
     @Select("SELECT id,cardCode,date FROM cardEve where cardCode like concat('%',#{cardCode},'%')\n" +
             "UNION\n" +
             "SELECT id,cardCode,date FROM cardMor where cardCode like concat('%',#{cardCode},'%')")
     List<CardEve> getCardAllByCode(String cardCode);
 
-//    //在考勤表 获得两个日期之间的查询内容 通过cardCode的 模糊查询
-//    @Select("select id, cardCode, name, date, isRepair " +
-//            "from card where cardCode like concat('%',#{cardCode},'%') " +
-//            "and date between #{preDate} and #{nextDate}")
-//    List<Card> getCardsByCodeByDate(@Param("cardCode") String cardCode,@Param("preDate") Date preDate,@Param("nextDate") Date nextDate);
-
-
     //插入数据打卡单
     @Insert("insert into cardEve (cardCode, name, date ,note ) " +
             "values(#{cardCode}, #{name}, #{date}, #{note}) ")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     Boolean insertCardEve(CardEve card);
-
 
     //更新数据 补卡单
     @Update("update cardEve set cardCode=#{cardCode}, name=#{name}, date=#{date}, note=#{note} " +
--- src/main/java/com/attend/dream/mapper/CardMorMapper.java
+++ src/main/java/com/attend/dream/mapper/CardMorMapper.java
@@ -8,38 +8,21 @@ import java.util.List;
 
 public interface CardMorMapper {
 
-
-
     //通过id来查询 打卡单
     @Select("select id, cardCode, name, date, note " +
             "from cardMor where id = #{id}")
     CardMor getCardMorById(int id);
-
-//    //用于结算当天的出勤情况
-//    @Select("select id,cardCode,name,date,note" +
-//            "from card where cardCode = #{cardCode}")
-//    List<Card> getCardsToday(String cardCode);
-
 
     //在打卡单  通过cardCode的 模糊查询
     @Select("select id, cardCode, name, date , note  " +
             "from cardMor where cardCode like concat('%',#{cardCode},'%') ")
     List<CardMor> getCardMorByCode(String cardCode);
 
-
-//    //在考勤表 获得两个日期之间的查询内容 通过cardCode的 模糊查询
-//    @Select("select id, cardCode, name, date, isRepair " +
-//            "from card where cardCode like concat('%',#{cardCode},'%') " +
-//            "and date between #{preDate} and #{nextDate}")
-//    List<Card> getCardsByCodeByDate(@Param("cardCode") String cardCode,@Param("preDate") Date preDate,@Param("nextDate") Date nextDate);
-
-
     //插入数据打卡单
     @Insert("insert into cardMor (cardCode, name, date ,note ) " +
             "values(#{cardCode}, #{name}, #{date}, #{note}) ")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     Boolean insertCardMor(CardMor card);
-
 
     //更新数据 补卡单
     @Update("update cardMor set cardCode=#{cardCode}, name=#{name}, date=#{date}, note=#{note} " +
