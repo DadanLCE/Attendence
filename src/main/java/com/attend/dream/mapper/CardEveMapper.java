6311a1a6b18a626945dae208f3239770a1f43be0
cc <516029781@qq.com>
2019-06-25 14:36:35 +0800

    cardMor 2.0

--- src/main/java/com/attend/dream/mapper/CardEveMapper.java
+++ src/main/java/com/attend/dream/mapper/CardEveMapper.java
@@ -1,6 +1,5 @@
 package com.attend.dream.mapper;
 
-import com.attend.dream.domain.Card;
 import com.attend.dream.domain.CardEve;
 import org.apache.ibatis.annotations.*;
 
@@ -24,7 +23,7 @@ public interface CardEveMapper {
     //在打卡单  通过cardCode的 模糊查询
     @Select("select id, cardCode, name, date , note  " +
             "from cardEve where cardCode like concat('%',#{cardCode},'%') ")
-    List<Card> getCardEvesByCode(String cardCode);
+    List<CardEve> getCardEvesByCode(String cardCode);
 
 
 //    //在考勤表 获得两个日期之间的查询内容 通过cardCode的 模糊查询
@@ -38,13 +37,13 @@ public interface CardEveMapper {
     @Insert("insert into cardEve (cardCode, name, date ,note ) " +
             "values(#{cardCode}, #{name}, #{date}, #{note}) ")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    Boolean insertCardEve(Card card);
+    Boolean insertCardEve(CardEve card);
 
 
     //更新数据 补卡单
     @Update("update cardEve set cardCode=#{cardCode}, name=#{name}, date=#{date}, note=#{note} " +
             "where id = #{id}")
-    Boolean updateCard(Card card);
+    Boolean updateCard(CardEve card);
 
     //删除数据 打卡单
     @Delete("delete from cardEve where id=#{id}")
--- src/main/java/com/attend/dream/mapper/CardMorMapper.java
+++ src/main/java/com/attend/dream/mapper/CardMorMapper.java
@@ -1,7 +1,7 @@
 package com.attend.dream.mapper;
 
-import com.attend.dream.domain.Card;
 import com.attend.dream.domain.CardEve;
+import com.attend.dream.domain.CardMor;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
@@ -13,7 +13,7 @@ public interface CardMorMapper {
     //通过id来查询 打卡单
     @Select("select id, cardCode, name, date, note " +
             "from cardMor where id = #{id}")
-    CardEve getCardMorById(int id);
+    CardMor getCardMorById(int id);
 
 //    //用于结算当天的出勤情况
 //    @Select("select id,cardCode,name,date,note" +
@@ -24,7 +24,7 @@ public interface CardMorMapper {
     //在打卡单  通过cardCode的 模糊查询
     @Select("select id, cardCode, name, date , note  " +
             "from cardMor where cardCode like concat('%',#{cardCode},'%') ")
-    List<Card> getCardMorByCode(String cardCode);
+    List<CardMor> getCardMorByCode(String cardCode);
 
 
 //    //在考勤表 获得两个日期之间的查询内容 通过cardCode的 模糊查询
@@ -38,13 +38,13 @@ public interface CardMorMapper {
     @Insert("insert into cardMor (cardCode, name, date ,note ) " +
             "values(#{cardCode}, #{name}, #{date}, #{note}) ")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    Boolean insertCardMor(Card card);
+    Boolean insertCardMor(CardMor card);
 
 
     //更新数据 补卡单
     @Update("update cardMor set cardCode=#{cardCode}, name=#{name}, date=#{date}, note=#{note} " +
             "where id = #{id}")
-    Boolean updateCard(Card card);
+    Boolean updateCard(CardMor card);
 
     //删除数据 打卡单
     @Delete("delete from cardMor where id=#{id}")
