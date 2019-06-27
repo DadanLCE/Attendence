cf31d0c01160fbdf75dea7ab13bd91c05687bc1e
cc <516029781@qq.com>
2019-06-27 14:06:51 +0800

    Pay1.0

--- src/main/java/com/attend/dream/controller/PayController.java
+++ src/main/java/com/attend/dream/controller/PayController.java
@@ -6,6 +6,7 @@ import com.attend.dream.service.PayService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,24 +20,45 @@ import java.util.Map;
 public class PayController {
     @Autowired
     private PayService payService;
+//
+//
+//    //查询，对应前端tbody
+//    @RequestMapping(value = "/pay", method = RequestMethod.POST)
+//    @ResponseBody
+//    public Map<Object, Object> getPaysByempCode(@RequestParam(value = "currentPage") int currentPage,
+//                                                    @RequestParam(value = "pageSize", defaultValue = "5") int pageSize, @RequestParam(value = "empCode") String empCode){
+//        PageInfo<Pay> paysPage = payService.getPaysByEmpCodePage(currentPage,pageSize,empCode);
+//        List<Pay> pays = payService.getPaysByEmpCode(currentPage,pageSize,empCode);
+//        int prePage = paysPage.getPrePage();
+//        int nextPage = paysPage.getNextPage();
+//        int pageNum = paysPage.getPages();
+//
+//        Map<Object, Object> payMap = new HashMap();
+//        payMap.put("pays", pays);
+//        payMap.put("nextPage", nextPage);
+//        payMap.put("prePage", prePage);
+//        payMap.put("pageNum", pageNum);
+//        return payMap;
+//    }
 
-
-    //查询，对应前端tbody
+    //查询员工通过名字 模糊查询 分页
     @RequestMapping(value = "/pay", method = RequestMethod.POST)
     @ResponseBody
-    public Map<Object, Object> getPaysByempCode(@RequestParam(value = "currentPage") int currentPage,
-                                                    @RequestParam(value = "pageSize", defaultValue = "5") int pageSize, @RequestParam(value = "empCode") String empCode){
-        PageInfo<Pay> paysPage = payService.getPaysByEmpCodePage(currentPage,pageSize,empCode);
-        List<Pay> pays = payService.getPaysByEmpCode(currentPage,pageSize,empCode);
+    public Map<Object, Object> getPaysByCode(Model model, @RequestParam(value = "currentPage") int currentPage,
+                                     @RequestParam(value = "pageSize", defaultValue = "5") int pageSize, @RequestParam(value = "empCode") String empCode) {
+        PageInfo<Pay> paysPage = payService.getPaysByEmpCodePage(currentPage, pageSize, empCode);
+        List<Pay> pays = payService.getPaysByEmpCode(currentPage, pageSize, empCode);
         int prePage = paysPage.getPrePage();
         int nextPage = paysPage.getNextPage();
         int pageNum = paysPage.getPages();
-
         Map<Object, Object> payMap = new HashMap();
         payMap.put("pays", pays);
         payMap.put("nextPage", nextPage);
         payMap.put("prePage", prePage);
         payMap.put("pageNum", pageNum);
+
         return payMap;
     }
+
+
 }
--- src/main/java/com/attend/dream/service/PayService.java
+++ src/main/java/com/attend/dream/service/PayService.java
@@ -2,6 +2,7 @@ package com.attend.dream.service;
 
 import com.attend.dream.domain.Card;
 import com.attend.dream.domain.Pay;
+import com.attend.dream.mapper.CheckCardMapper;
 import com.attend.dream.mapper.PayMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -15,6 +16,9 @@ import java.util.List;
 public class PayService {
     @Autowired
     private PayMapper payMapper;
+
+    @Autowired
+    private CheckCardMapper checkCardMapper;
     //模糊查询
     //查询页面的信息 上一页 下一页
     public PageInfo<Pay> getPaysByEmpCodePage(int currentPage, int pageSize, String empCode){
@@ -35,8 +39,58 @@ public class PayService {
         int days = (int) ((nextDate.getTime() - preDate.getTime()) / (1000*3600*24));
         return days;
     }
-    public int salary(Card card){
-        days = countTime(card.get)
+    public int salary(String empCode){
+        int[] k = new int[12];//旷工
+        int[] d = new int[12];//迟到 早退 不正常
+        int[] z = new int[12];//准时
+        Pay pay = new Pay();
+        List<Card> cards = checkCardMapper.getCardByCode(empCode);
+
+        Date edate = cards.get(0).getMorTime(); //最早时间
+        Date ldate = cards.get(0).getEveTime(); //最晚时间
+        int aflag= edate.getMonth();
+        int flag = edate.getMonth();
+        for(Card a : cards){
+            if(edate.compareTo(a.getMorTime())!= -1){
+                edate = a.getMorTime();
+            }
+        }
+        for(Card a : cards){
+            if(ldate.compareTo(a.getMorTime())== -1){
+                ldate = a.getMorTime();
+            }
+        }
+
+            //aflag值为开始计算工资的月份，flag为最后计算工资的月份
+            //从最早月份开始遍历
+            for(int i = edate.getMonth();i <= ldate.getMonth();i++){
+
+                    for(Card c : cards){
+                        if(c.getNote()!=null) {
+                            if (c.getMorTime().getMonth() == i) {
+                                if (c.getNote().equals("旷工")) {
+                                    k[flag]++;
+                                } else if (c.getNote().equals("不正常")) {
+                                    d[flag]++;
+                                } else {
+                                    z[flag]++;
+                                }
+                            }
+                        }
+                    }
+                flag++;
+            }
+
+            for (int i = aflag; i<flag; i++){
+                int salary = d[i]*50+z[i]*100;
+                pay.setStartDay(edate);
+                pay.setEndDay(ldate);
+                pay.setSalary(salary);
+                pay.setEmpCode(empCode);
+                pay.setName(cards.get(0).getName());
+                payMapper.insertPay(pay);
+            }
+
         return 0;
     }
 }
