092a5f2b4d1ac26d6f78a8ecefab65257ad67ee4
DandanLCE <www.chenjingq@qq.com>
2019-06-27 14:09:16 +0800

    123123123

--- src/test/java/com/attend/dream/DreamApplicationTests.java
+++ src/test/java/com/attend/dream/DreamApplicationTests.java
@@ -4,6 +4,10 @@ import com.attend.dream.domain.*;
 import com.attend.dream.mapper.*;
 import com.attend.dream.service.ClassesService;
 import com.attend.dream.service.DepartmentService;
+
+import com.attend.dream.service.StationService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
@@ -20,158 +24,10 @@ import java.util.List;
 @SpringBootTest
 @MapperScan(basePackages = {"com.attend.dream.mapper"})
 public class DreamApplicationTests {
-//    @Autowired
-//    private EmployeesMapper employeesMapper;
 
 
-    @Test
-    public void contextLoads() {
-
-//        Employee e = employeesMapper.getEmployeeByName("方少少");
-
-//        PageHelper.startPage(2,2);
-//        List<Employee> allE = employeesMapper.getEmployees();
-//        PageInfo<Employee> pageInfo = new PageInfo<>(allE);
-//        System.out.println(allE);
-//        for ( Employee e: pageInfo
-//             ) {
-//            System.out.println(e);
-//
-//        }
-//        for (Employee e:
-//             allE) {
-//            System.out.println(e);
-//        }
-
-//        System.out.println(pageInfo);
-//        PageInfo<Employee> pageInfo = new PageInfo<>(allE);
+    public void test(){
+        Date date = new Date();
 
     }
-    @Autowired
-    private DepartmentMapper departmentMapper;
-    @Autowired
-    private DepartmentService departmentService;
-
-    @Autowired
-    private StationMapper s;
-
-    @Test
-    public void departmentT(){
-        String code = null;
-
-        Station sta = s.getStationById(11);
-        sta.setStaCode("TTA");
-        s.insertStation(sta) ;
-        System.out.println(sta);
-    }
-    @Autowired
-    private ClassesMapper classesMapper;
-    @Autowired
-    private ClassesService classesService;
-    @Test
-    public void claTest(){
-//        List<Classes> list = classesService.getClassesByCode(1,3,"a");
-//        PageInfo<Classes> pageInfo = classesService.getClassesByCodePage(1,3,"a");
-//        Classes c = classesService.getClaById(3);
-//        c.setClaCode("POSADLAKSD");
-//        String a = classesService.insertCla(c);
-//        System.out.println(c);
-//        System.out.println(a);
-//        c.setId(10);
-//        String a = classesService.insertCla(c);
-//        System.out.println(a);
-//        System.out.println(pageInfo);
-//        for (Classes c:
-//             list) {
-//            System.out.println(c);
-//        }
-//        String dateStr = "2010/05/04 12:34:23";
-        //注意format的格式要与日期String的格式相匹配
-//        DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//        try {
-//            date = sdf.parse(dateStr);
-//            System.out.println(date.toString());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-        String dateStr = "2015-02-10 22:00:00";
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date d1;
-        try {
-            d1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateStr);
-            System.out.println("DateTime d1>>>>>>: " + d1);
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-//
-//
-//        Card card = new Card();
-//        card.setDate(date);
-//        System.out.println(card.getDate());
-
-    }
-
-
-
-    @Test
-    public void cardT(){
-//        List<RepairCard> c = repairCardMapper.getCardsByCode("b");
-
-//        Date date = null;
-//        Date date2 = null;
-////        注意format的格式要与日期String的格式相匹配
-//        String dateStr = "2019-08-01 00:00:00";
-//        String dateStr2 = "2019-08-20 00:00:00";
-//        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        try {
-//            date = sdf.parse(dateStr);
-//            date2 = sdf.parse(dateStr2);
-//            System.out.println(date2.toString());
-//            System.out.println(date.toString());
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        List<CardEve> list = cardEveMapper.getAll("",date,date2);
-//        System.out.println(list);
-//        for (int i =0; i < list.size();i++){
-//            System.out.println(list.get(i));
-//        }
-//        cardMorMapper.insertCardMor(card);
-//        System.out.println(list);
-//        CardMor card1 = cardMorMapper.getCardMorById(2);
-//        System.out.println(card1);
-//        card1.setName("哇啦啦");
-//        cardMorMapper.updateCard(card1);
-//        CardMor cardMor = cardMorMapper.getCardMorByCode("AA");
-
-    }
-    @Autowired
-    private  CheckCardMapper checkCardMapper;
-    @Test
-    public void rCard(){
-                Date date = null;
-        Date date2 = null;
-//        注意format的格式要与日期String的格式相匹配
-        String dateStr = "2019-08-01 00:00:00";
-        String dateStr2 = "2019-08-20 00:00:00";
-        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        try {
-            date = sdf.parse(dateStr);
-            date2 = sdf.parse(dateStr2);
-            System.out.println(date2.toString());
-            System.out.println(date.toString());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        List<Card> list = checkCardMapper.getCardsByCodeTime("",date, date2);
-        System.out.println(list);
-    }
-
-
-
 }
